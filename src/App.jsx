b12760f58d1cfd929d5b772f183d84cb5d343025
Voramet Borvornsirisan <u6610830@au.edu>
2025-12-19 10:59:05 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css' 
+
+import Greetings from './components/Greetings' 
+
+import User from './components/User' 
+
+function App() { 
+
+  return ( 
+
+    <div className="card"> 
+
+     <Greetings/> 
+
+    </div> 
+
+  ) 
+
+} 
+
+export default App 
---
+++ src/Greetings.jsx
@@ -0,0 +1,19 @@
+import "../App.css" 
+
+function Greetings() { 
+
+  return ( 
+
+    <div> 
+
+   <h1>Hello, React!</h1> 
+
+ <p>Voramet Borvornsirisan</p> 
+
+</div> 
+
+); 
+
+} 
+
+export default Greetings; 
